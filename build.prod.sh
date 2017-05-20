54c7cf934460a03ca1b86ce55a058301b07015fd
kboom <gurgul.grzegorz@gmail.com>
2017-05-20 11:19:33 +0200

    Travis build stages

--- build.prod.sh
+++ build.prod.sh
@@ -8,7 +8,7 @@ export SERVER_API_URL=http://192.168.99.100:2701/api
 ROOT_DIR=$(pwd)
 
 cd ${ROOT_DIR}/JustMarriedClient
-./build.sh
+./do.build.sh
 
 cd ${ROOT_DIR}/JustMarriedServer
-./build.sh
+./do.build.sh
